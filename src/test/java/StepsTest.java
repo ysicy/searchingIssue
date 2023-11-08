c60c718174586077710fb6217222a8845a99e40b
ysicy <alexeyecko@gmail.com>
2023-11-08 15:29:23 +0300

    workinnnn

--- src/test/java/StepsTest.java
+++ src/test/java/StepsTest.java
@@ -1,12 +1,13 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
 public class StepsTest{
@@ -19,7 +20,11 @@ public class StepsTest{
 
 
     @Test
-    @DisplayName("Проверка Issue в репозитории junitHomeWork с Listener")
+    @Feature("Поиск в репозитории")
+    @Story("Поиск в репозитории junitHomeWor")
+    @Owner("ysicy")
+    @Severity(SeverityLevel.BLOCKER)
+    @DisplayName("Проверяем Issue в репозитории junitHomeWork с Listener")
     public void checkNameIssue() {
         open(URL);
         $(LOCATOR).click();
@@ -28,8 +33,40 @@ public class StepsTest{
         $("#issues-tab").click();
         $(withText(ISSUETEXT)).should(Condition.exist);
     }
+    @Feature("Поиск в репозитории")
+    @Story("Поиск в репозитории junitHomeWor")
+    @Owner("ysicy")
+    @Severity(SeverityLevel.BLOCKER)
+    @DisplayName("Проверяем Issue в репозитории junitHomeWork с помощьюlambda")
     @Test
-    @DisplayName("Проверка Issue в репозитории junitHomeWork по шагам с аннотацией @Step")
+    public void lambdaStep () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу github", () -> {
+            open("https://github.com/");
+        });
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $x("//div[contains(@class,'mr-2 color-fg-muted')]").click();
+            $x("//input[contains(@id,'query-builder-test')]").sendKeys(REPOSITORY);
+            $x("//input[contains(@id,'query-builder-test')]").pressEnter();
+        });
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Кликаем по таб Issue", () -> {
+            $x("//a[contains(@id,'issues-tab')]").click();
+        });
+        step("Проверяем наличие текста " + ISSUETEXT, () -> {
+            $x("//div[contains(@class,'container-md')]").shouldHave(Condition.text(ISSUETEXT));
+        });
+    }
+
+    @Test
+    @Feature("Поиск в репозитории")
+    @Story("Поиск в репозитории junitHomeWor")
+    @Owner("ysicy")
+    @Severity(SeverityLevel.BLOCKER)
+    @DisplayName("Проверяем Issue в репозитории junitHomeWork по шагам с аннотацией @Step")
     public void searchIssueTest(){
 
         SelenideLogger.addListener("allure", new AllureSelenide());
@@ -43,4 +80,6 @@ public class StepsTest{
     }
 
 }
+
+
 
